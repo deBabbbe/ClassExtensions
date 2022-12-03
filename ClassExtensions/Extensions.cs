eefa0a8349d8fd8da3a70b23d62c925a9634c399
deBabbbe <neotbh@freenet.de>
2022-12-03 23:19:27 +0100

    Refactor ForAll

--- ClassExtensions/Extensions.cs
+++ ClassExtensions/Extensions.cs
@@ -5,20 +5,15 @@ namespace ClassExtensions;
 
 public static class Extensions
 {
-    public static bool IsNull(this object @object) =>
-        @object == null;
+    public static bool IsNull(this object @object) => @object == null;
 
-    public static bool IsNotNull(this object @object) =>
-        !@object.IsNull();
+    public static bool IsNotNull(this object @object) => !@object.IsNull();
 
     public static void ForAll<T>(this IEnumerable<T> enumerable, Action<T> action)
     {
         if (enumerable.IsNull() || action.IsNull()) return;
 
-        foreach (var entry in enumerable)
-        {
-            action(entry);
-        }
+        enumerable.ToList().ForEach(action);
     }
 
     public static void Times(this int count, Action<int> action) =>
